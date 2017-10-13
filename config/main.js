aa0214019805b67125ae013be2509c48bd85183e
Zach Durham <ZachDurham@Zachs-MacBook-Pro.local>
2017-10-13 18:14:54 -0400

    simple typo fix

--- config/main.js
+++ config/main.js
@@ -2,7 +2,7 @@ module.exports = {
   // Secret key for JWT signing and encryption
   'secret': 'N Y T R E A C T',
   // Database connection information
-  'database': MONGODB_URI || 'mongodb://localhost:27017/nytreact',
+  'database': process.env.MONGODB_URI || 'mongodb://localhost:27017/nytreact',
   // Setting port for server
   'port': process.env.PORT || 3001
 }
